92d227bfbf488bbd35137dcc85338bb7a442caee
César de la Cal Bretschneider <cesar@magic3w.com>
2019-09-11 09:10:58 +0200

    API prototype with localization and autocomplete
    Summary:
    BROKEN: Depends on changes in m3.js that are not upstream
    This prototype allows for a basic interaction with the API to explore it's functionality
    
    Test Plan: Review the code, reject until the submodule is integrated
    
    Reviewers: sebastian
    
    Reviewed By: sebastian
    
    Differential Revision: https://phabricator.magic3w.com/D95

--- assets/js/autocomplete.js
+++ assets/js/autocomplete.js
@@ -7,7 +7,7 @@ depend(['m3/core/collection'], function (collect) {
 
 		this.render = function (wrapper) {
 			var opt = this.html = wrapper.appendChild(document.createElement('div'));
-			opt.className = 'autocomplete-result'
+			opt.className = 'autocomplete-result';
 			opt.value = k;
 			opt.innerHTML = v;
 
@@ -22,10 +22,10 @@ depend(['m3/core/collection'], function (collect) {
 
 		this.remove = function () {
 			this.html.parentNode.removeChild(this.html);
-		}
-	}
+		};
+	};
 
-	var autocomplete = function (element, callable) {
+	var Autocomplete = function (element, callable) {
 		//Register listeners
 		//Provide a callback
 		//Provide mechanism to unregister the listeners
@@ -40,8 +40,8 @@ depend(['m3/core/collection'], function (collect) {
 
 		this.dummy.type = 'text';
 		this.dummy.className = 'autocomplete-input';
-		this.anchor.className = 'autocomplete-results-anchor'
-		this.drop.className = 'autocomplete-results autocomplete-hidden'
+		this.anchor.className = 'autocomplete-results-anchor';
+		this.drop.className = 'autocomplete-results autocomplete-hidden';
 
 		element.parentNode.insertBefore(this.dummy,element);
 		element.parentNode.insertBefore(this.anchor,element);
@@ -63,17 +63,17 @@ depend(['m3/core/collection'], function (collect) {
 			}
 
 			this.reset();
-
-			collect(rss).each(function (e) { e.render(this.drop); });
+			
+			collect(rss).each(function (e) { e.render(self.drop); });
 			this.rendered = rss;
 			this.drop.classList.remove('autocomplete-hidden');
-		}
+		};
 
 		this.reset = function () {
 			this.rendered? collect(this.rendered).each(function (e) { e.remove(); }) : null;
 			this.rendered = undefined;
 			this.drop.classList.add('autocomplete-hidden');
-		}
+		};
 
 		this.dummy.addEventListener('keyup', function (e) {
 			callable(this.value, function (result) {
@@ -99,9 +99,9 @@ depend(['m3/core/collection'], function (collect) {
 
 		document.addEventListener('click', function () {
 			self.reset();
-		})
+		});
 	};
 
-	return autocomplete;
+	return function (e, cb) { return new Autocomplete(e, cb); };
 
 });
